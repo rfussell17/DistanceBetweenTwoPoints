74c73d53d3e980640ad475c703b9f47bc1f192f0
Robin Fussell <rfussell17@gmail.com>
2020-06-18 17:02:12 -0700

    added test class

---
+++ Main.java
@@ -0,0 +1,12 @@
+package com.rfussell;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main (String[] args) {
+
+        System.out.println("Please enter values for x1, y1, x2, y2 ");
+        System.out.printf("the distance is : %s%n", CartesianCoordinateSystem.calculateDistance());
+    }
+}
