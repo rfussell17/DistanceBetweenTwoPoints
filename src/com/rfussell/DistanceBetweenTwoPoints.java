46803dc8d2359015fc4b51d458fba9a376921380
Robin Fussell <rfussell17@gmail.com>
2020-06-18 17:01:10 -0700

    Update DistanceBetweenTwoPoints.java
    refactored code

--- src/com/rfussell/DistanceBetweenTwoPoints.java
+++ src/com/rfussell/DistanceBetweenTwoPoints.java
@@ -1,29 +1,57 @@
 package com.rfussell;
 
-/**
- * Class: DistanceBetweenTwoPoints.java
- * Purpose: Write a program that calculates the distance between two points.
- * @author: Robin Fussell
- * Date:  October 1, 2019
- * Version
- * */
-
 import java.util.Scanner;
-import java.lang.Math;
 
+/**
+ * Class: CatesianCoordinateSystem.java
+ *
+ * Test Class: Main.java
+ *
+ * Purpose: Write a program that calculates the distance between two points.
+ *
+ *
+ * ------------------------------------------
+ Please enter values for x1, y1, x2, y2
+ -2
+ -3
+ -4
+ 4
+ the distance is : 7.28
 
-public class DistanceBetweenTwoPoints {
-    public static void main(String[] args) {
+ Process finished with exit code 0
+ * ------------------------------------------
+ *
+ *
+ * @author: Robin Fussell
+ * Date:  June 18, 2020
+ * Version 2.0
+ */
+
+public class CartesianCoordinateSystem {
+
+//create method to calculate the distance
+
+    public static double calculateDistance () {
+
+        //create scanner to take input for the 4 points to be calculated
+        //assign the inputs be used in calculation
+
         Scanner userInput1 = new Scanner(System.in);
-        System.out.println("Please enter values for x1, y1: ");
         double x1 = userInput1.nextDouble();
         double y1 = userInput1.nextDouble();
+
         Scanner userInput2 = new Scanner(System.in);
-        System.out.println("Please enter values for x2, y2: ");
         double x2 = userInput2.nextDouble();
         double y2 = userInput2.nextDouble();
 
-        double distance = Math.sqrt(Math.pow(x2 - x1,2) + Math.pow(y2 - y1,2) * 1.0);
-        System.out.printf("the distance is : %s%n", distance);
+        //calculate distance using the formula provided
+
+        double distance = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2) * 1.0);
+
+        //use a roundedDistance variable to store the rounded and truncated number to be returned
+
+        double roundedDistance = Math.round(distance*100)/100d;
+
+        return roundedDistance;
     }
 }
